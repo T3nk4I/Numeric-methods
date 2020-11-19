dff8785808d85ae08b3e72c1c73c84a2c7346613
T3nk4I <66106584+T3nk4I@users.noreply.github.com>
2020-11-19 23:28:13 +0900

    Update Final.cpp

--- Final.cpp
+++ Final.cpp
@@ -6,6 +6,9 @@
 #include <cmath>
 #include <C:\Users\leone\Documents\Metodos numericos\P020.h>
 #include <C:\Users\leone\Documents\Metodos numericos\Metodo del trapecio.h>
+#include <C:\Users\leone\Documents\Metodos numericos\trapmult.h>
+#include <C:\Users\leone\Documents\Metodos numericos\simp13.h>
+
 using namespace std;
 
 int main(){
@@ -15,28 +18,31 @@ int main(){
 		textcolor(WHITE);
 		textbackground(BLUE);
 		gotoxy(35,9); cprintf("Menu");
-		gotoxy(10,10); cprintf("1,t,T: Metodo del trapecio");
-		gotoxy(10,11); cprintf("2,c,C: ");
-		gotoxy(10,12); cprintf("3,f,F: Fin");
+		gotoxy(10,10); cprintf("1. Metodo del trapecio");
+		gotoxy(10,11); cprintf("2. Metodo del trapecio multiple");
+		gotoxy(10,12); cprintf("3. Metodo de simpson %c simple",8531);
+		gotoxy(10,13); cprintf("4. Fin");
 		gotoxy(10,22); cprintf("Inserte opcion que desea realizar: ");
 		cin>> opc;
 		switch(opc){
 			case '1':
-			case 'T':
-			case 't':
 				P020();
 				trapeciosim();
 				getch();
 				break;
 			case '2':
-			case 'C':
-			case 'c':
 				P020();
+				trapmult();
 				getch();
 				break;
-			case '3':
-			case 'F':
-			case 'f': 
+			case '3': 
+				P020();
+				simp13();
+				P020(); 
+				res();
+				getch();
+				break;
+			case '4':
 				goto fin;
 				break;
 			default:
