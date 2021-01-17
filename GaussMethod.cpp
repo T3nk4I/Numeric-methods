b2d4be7ea8641287cb40653ea54a59184280feb9
T3nk4I <66106584+T3nk4I@users.noreply.github.com>
2021-01-17 18:05:00 +0900

    Update GaussMethod.cpp

--- GaussMethod.cpp
+++ GaussMethod.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <stdio.h>
 #include <conio.h>
 #include <windows.h>
+#include <stdio.h>
 using namespace std;
 
 int main(){
-	int m,n,k=4,l=12,A=0,b;
-	float a[10][10],nu[10][10],p;
+	int m,n,k=4,l=12,A=0,B=1;
+	float a[10][10],nu[10][10],p,c;
 	gotoxy(4,9);	cprintf("Teclee el numero de ecuaciones");
 	gotoxy(4,10);	cin>> m;
 	n=m+1;
@@ -18,53 +18,40 @@ int main(){
 		k=4;
 		l++;
 	}
-	l=l+2;
-	gotoxy(4,l);	cprintf("Impresion :");
-	l++;
 	for(int i=0; i<m; i++){
 		for(int j=0; j<n; j++){
-			if(j==n-1){
-				gotoxy(k,l);	cout<< "="<< a[i][j];
-			}
-			else{
-				if(j==0){
-					gotoxy(k,l);	cout<< a[i][j]<< "x"<< j+1;
-					k=k+5;
-				}
-				else{
-					if(a[i][j]>=0){
-						gotoxy(k,l);	cout<< "+"<< a[i][j]<< "x"<< j+1;
-						k=k+5;
-					}
-					else{
-						gotoxy(k,l);	cout<< a[i][j]<< "x"<< j+1;
-						k=k+5;
-					}
-				}
-			}
+			gotoxy(k,l);	cout<< a[i][j];
+			k=k+8;
 		}
 		k=4;
 		l++;
 	}
-	Sleep(1000);
 	system("cls");
+	k=4;
+	l=12;
 	do{
 		p=a[A][A];
-		for(int j=0; j<n; j++){
-			gotoxy(k,l);	cout<< a[A][j]/p;
-			Sleep(500);
-			k=k+5;
-		}
-		l++;
-		k=4;
-		for(int i=A; i<m-1; i++){
-			for(int j=0; j<n; j++){
+		for(int i=A; i<=A; i++){
+			for(int j=A; j<n; j++){
+				a[i][j]=a[i][j]/p;
 				gotoxy(k,l);	cout<< a[i][j];
-				k=k+5;
+				k=k+8;
 			}
 			k=4;
 			l++;
 		}
+		for(int i=B; i<m; i++){
+			c=a[A+B][A];
+			for(int j=0; j<n; j++){
+				a[i][j]=a[i][j]-c*a[A][j];
+				gotoxy(k,l);	cout<< a[i][j];
+				k=k+8;
+			}
+			k=4;
+			l++;
+			B++;
+		}
 		A++;
-	}while(A<1);
+	}while(A<m);
+	return 0;
 }
